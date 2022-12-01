749d97ec8251682e8074ea5fc58e373f8f0381aa
mikkohaario <mikko.haario1@gmail.com>
2022-12-01 11:08:08 +0200

    uusin mikon

--- lambda1.py
+++ lambda1.py
@@ -1,10 +1,11 @@
 import requests
 import json
 
-#Haetaan Digitraficin rajapinnasta kaikki myohassa olleet minuutit,juna-asema Tampere
-
 data = requests.get('https://rata.digitraffic.fi/api/v1/live-trains/station/TPE')
+data2 = requests.get('https://api.weatherapi.com/v1/current.json?key=b1de5ace880e458ba11220248223011&q=Tampere&aqi=no')
+
 data_dict = data.json()
+data2_dict = data2.json()
 
 station_sort = []
 late = []
@@ -19,33 +20,18 @@ for train in data_dict:
                 early.append(difference)
             elif difference > 0: 
                 late.append(difference) 
-# print(late)
-# print(early)
+
 late_all = sum(late)
 early_all = sum(early)
-# print(late_all)
-# print(early_all)
+
 late_hours = late_all / 60
 early_hours = (early_all /60) * -1
-round_late = round(late_hours, ndigits=1)
-round_early = round(early_hours, ndigits=1)
-
-print(f"Today the trains has been {round_late} hours late alltogether and {round_early} hours ahead of time")
-
-#print(kokonais)
+round_late = round(late_hours, ndigits= 1)
+round_early = round(early_hours, ndigits= 1)
 
 
-# for row in data_dict:
-#     station_sort.append([row['scheduledTime'], row['actualTime']])
-# station_sorted = sorted(station_sort, key=lambda tup: tup[0])
-# for row in station_sorted:
-#     print(f"{row[0]}. {row[1]}")
+value_saa2 = data2_dict['location']['name']
+value_saa = data2_dict['current']['temp_c']
 
 
-# ⭐Järjestä tuloksena saadut vastaukset ”forks” –arvon mukaan laskevassa järjestyksessä
-# forks_sort = []
-# for row in data_dict:
-#     forks_sort.append([row['stationName'], row['stationShortCode']])
-# forks_sorted = sorted(forks_sort, key=lambda tup: tup[0])
-# for row in forks_sorted:
-#     print(f"{row[0]}. {row[1]}")
+print(f"Today the train has been {round_late} hours late alltogether and {round_early} hours ahead of time and the current weather in Tampere is: {value_saa}")
